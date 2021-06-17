2a498850b75284326e4ce9df6d5c296d25004f51
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-17 18:51:54 -0400

    add mergeNestedList and removeDuplicateElement files

---
+++ mergeNestedList.js
@@ -0,0 +1,17 @@
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+
+// flat function https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+const lists = [[1,4,5],[1,3,4],[2,6]]
+const mergeNestedList = function(arr){
+  return arr.flat().sort();
+}
+console.log(mergeNestedList(lists));
+
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
+
+console.log(flatten(lists));
---
+++ removeDuplicateElement.js
@@ -0,0 +1,20 @@
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+const removeDuplicateElement = function (arr) {
+  const elements = [];
+  const underscore = []
+  for (const element of arr){
+    let result
+    if (elements.includes(element)){
+      underscore.push('_')
+    } else {
+      elements.push(element)
+    }
+  }
+  const result = elements.concat(underscore)
+  return `output: ${elements.length}, nums = [${result}]`
+}
+
+console.log(removeDuplicateElement(nums))
