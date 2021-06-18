fbc1816691dcba4293bf7603c96df19c31538d17
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-18 18:54:24 -0400

    add jest coverage

--- mergeNestedList.test.js
+++ mergeNestedList.test.js
@@ -2,4 +2,5 @@ const flatten = require('./mergeNestedList');
 
 test('test flatten function to nested array', () => {
   expect(flatten([1, 2, [2, 5]])).toStrictEqual([1, 2, 2, 5])
+  expect(flatten([1, 2, [2, 5, [7]]])).toEqual([1, 2, 2, 5, 7])
 })
