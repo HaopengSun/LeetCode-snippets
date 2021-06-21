cb2ac7e38a6ca56e53ea8c0ea47bb37ba29bf5d8
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-21 17:12:42 -0400

    group island in a matrix

--- removeIsland.js
+++ removeIsland.js
@@ -11,7 +11,7 @@ const edge = [];
 const removeIsland = function(matrix){
   for (let i = 0; i < matrix.length; i++){
     for (let j = 0; j < matrix.length; j++){
-      if (i === 0 || j === 0 || i === 5 || j === 5){
+      if (i === 0 || j === 0 || i === matrix.lengt - 1 || j === matrix.lengt - 1){
         edge.push(j.toString() + i.toString())
       }
     }
@@ -21,11 +21,12 @@ const removeIsland = function(matrix){
   console.log(edge)
 }
 
-const adjcent = function(x, y){
+const adjcent = function(matrix, x, y){
   const neighbours = [[x + 1, y], [x, y + 1], [x - 1, y], [x, y - 1]]
   const result = []
+  const edge = matrix.length
   for (const neighbour of neighbours){
-    if ((neighbour[0] >= 0 && neighbour[1] >= 0) && (neighbour[0] < 6 && neighbour[1] < 6)){
+    if ((neighbour[0] >= 0 && neighbour[1] >= 0) && (neighbour[0] < edge && neighbour[1] < edge)){
       let surrounding = neighbour[0].toString() + neighbour[1].toString()
       result.push(surrounding)
     }
@@ -36,13 +37,11 @@ const adjcent = function(x, y){
 const touchEdge = function(arr, matrix){
   let found = false
   for (const element of arr){
-    // console.log(matrix[element[0]][element[1]])
     if (matrix[element[0]][element[1]] === 1){
-      const neighbours = adjcent(Number(element[0]), Number(element[1]))
+      const neighbours = adjcent(matrix, Number(element[0]), Number(element[1]))
       for (const neighbour of neighbours){
         let x = Number(neighbour[0])
         let y = Number(neighbour[1])
-        console.log(matrix[x][y], x, y, !edge.includes(neighbour))
         if (matrix[x][y] === 1 && !edge.includes(neighbour)){
           found = true
           edge.push(neighbour)
@@ -54,5 +53,13 @@ const touchEdge = function(arr, matrix){
 }
 
 removeIsland(input)
-// console.log(adjcent(0, 0))
 
+// const nums = [0, 1, 2, 3]
+// for (const num of nums){
+//   let newNum = 7 - num
+//   if ( newNum > 0 && !nums.includes(newNum)){
+//     nums.push(newNum)
+//   }
+// }
+// console.log(nums)
+// output [ 0, 1, 2, 3, 7, 6, 5, 4 ]
