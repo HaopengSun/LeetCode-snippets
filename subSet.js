6c282ffb3df55406f430fa4907307d6f1188caaf
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-25 15:49:57 -0400

    another method to do unrepeated permutations

--- subSet.js
+++ subSet.js
@@ -24,3 +24,21 @@ console.log(subsets(nums))
 // [], [1]
 // [], [1], [2], [1, 2]
 // [], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]
+
+const reducer = (accumulator, currentValue) => accumulator.toString() + currentValue.toString()
+const setSets = []
+const subSet = function(arr, current){
+  arr.map(element => {
+    if (element){
+      let newResult = [...current, element]
+      if (!setSets.includes(newResult.sort().reduce(reducer))) setSets.push(newResult.sort().reduce(reducer).toString())
+      let newArr = arr.filter(e => e !== element)
+      subSet(newArr, newResult)
+    }
+  })
+}
+
+subSet(nums, [])
+const result = [[]]
+setSets.map(letter => result.push(letter.split('').map(num => Number(num))))
+console.log(result)
