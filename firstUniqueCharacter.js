caa706a78777997d04fe2d51f77ef9b6498913bc
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-28 11:07:44 -0400

    add another solution to return first unique letter in a string

--- firstUniqueCharacter.js
+++ firstUniqueCharacter.js
@@ -7,19 +7,30 @@
 // Input: s = "aabb"
 // Output: -1
 
+// const firstUniqueCharacter = function(str){
+//   const arrLetter = str.split('')
+//   const uniqueLetter = []
+//   const repeatLetter = []
+//   arrLetter.forEach(letter => {
+//     if (uniqueLetter.includes(letter)) {
+//       repeatLetter.push(letter)
+//     } else {
+//       uniqueLetter.push(letter)
+//     }
+//   })
+//   for(let i = 0; i < uniqueLetter.length; i++){
+//     if (!repeatLetter.includes(uniqueLetter[i])) return i
+//   }
+//   return -1
+// }
+
 const firstUniqueCharacter = function(str){
-  const arrLetter = str.split('')
-  const uniqueLetter = []
-  const repeatLetter = []
-  arrLetter.forEach(letter => {
-    if (uniqueLetter.includes(letter)) {
-      repeatLetter.push(letter)
-    } else {
-      uniqueLetter.push(letter)
+  for (let i = 0; i < str.length; i++){
+    let found = false
+    for (let j = 0; j < str.length; j++){
+      if (i !== j && str[i] === str[j]) found = true
     }
-  })
-  for(let i = 0; i < uniqueLetter.length; i++){
-    if (!repeatLetter.includes(uniqueLetter[i])) return i
+    if (!found) return i
   }
   return -1
 }
