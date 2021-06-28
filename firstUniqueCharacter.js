ecae8b59af2d45bb5916fe1dc1d3c85a996c8136
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-28 10:47:47 -0400

    function returns the first unique letter in a string

---
+++ firstUniqueCharacter.js
@@ -0,0 +1,32 @@
+// Input: s = "leetcode"
+// Output: 0
+
+// Input: s = "loveleetcode"
+// Output: 2
+
+// Input: s = "aabb"
+// Output: -1
+
+const firstUniqueCharacter = function(str){
+  const arrLetter = str.split('')
+  const uniqueLetter = []
+  const repeatLetter = []
+  arrLetter.forEach(letter => {
+    if (uniqueLetter.includes(letter)) {
+      repeatLetter.push(letter)
+    } else {
+      uniqueLetter.push(letter)
+    }
+  })
+  for(let i = 0; i < uniqueLetter.length; i++){
+    if (!repeatLetter.includes(uniqueLetter[i])) return i
+  }
+  return -1
+}
+
+const str = "leetcode"
+console.log(firstUniqueCharacter(str))
+const str1 = "loveleetcode"
+console.log(firstUniqueCharacter(str1))
+const str2 = "aabb"
+console.log(firstUniqueCharacter(str2))
