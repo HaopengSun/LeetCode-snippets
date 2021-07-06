a9816d355f1e8ac6e8494f95a73988a5a37d6973
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-06 11:43:21 -0400

    symmetric tree

--- tree.js
+++ tree.js
@@ -23,3 +23,16 @@ const maxDepth = function(root) {
   goDown(root, 1)
   return answer
 };
+
+// whether a tree is symmetric
+const isSymmetric = function(root) {
+  return helper(root, root)
+};
+
+const helper = function(p, q) {
+  if (p === null && q === null)
+      return true
+  if (p === null || q === null)
+      return false
+  return p.val === q.val && helper(p.left, q.right) && helper(p.right, q.left)
+}
