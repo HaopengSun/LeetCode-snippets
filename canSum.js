f5451e3c156baa39a4f6cc9fa9ed19728952bf11
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-23 17:45:51 -0400

    recursive solution returns only true or false of the sum

--- canSum.js
+++ canSum.js
@@ -17,3 +17,18 @@ const canSum = (nums, target, memo = []) => {
 
 canSum([2, 3, 4, 7, 5], 7)
 console.log(result)
+
+
+// recursive solution and the elemenet in an array can be reused
+// only return true or false
+const canSum1 = (target, nums) => {
+  if (target === 0) return true
+  if (target < 0) return false
+  for (let num of nums){
+    let remainder = target - num
+    if (canSum1(remainder, nums) === true) return true
+  }
+  return false
+}
+
+console.log(canSum1(7, [2, 3, 4, 7, 5]))
