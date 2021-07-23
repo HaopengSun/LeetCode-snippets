25d8b0fad004a47762d7a331c1897c3ac31924a5
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-22 22:34:12 -0400

    while loop to generate pascals triangle

---
+++ pascalsTriangle.js
@@ -0,0 +1,29 @@
+// Input: rowIndex = 3
+// Output: [1, 3, 3, 1]
+
+// Input: rowIndex = 4
+// Output: [1 ,4, 6, 4, 1]
+
+const stepDownward = arr => {
+  const result = [1]
+  for (let i = 0; i < arr.length - 1; i++){
+    result.push(arr[i] + arr[i + 1])
+  }
+  result.push(1)
+  return result
+}
+
+const pascalsTriangle = rowIndex => {
+  if (rowIndex === 1) return [1, 1]
+  let result = [1, 1]
+  if (rowIndex > 2){
+    while (rowIndex > 1){
+      result = stepDownward(result)
+      rowIndex -= 1
+    }
+  }
+  return result
+}
+
+// console.log(stepDownward([1, 2, 1]))
+console.log(pascalsTriangle(4))
