a8de04a0a8b3c8b3f68821508098c42547aeb57e
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-24 15:22:58 -0400

    indexOf and slice to ensure the selected words are prefix and memo to record whether the subWord can be found in the array

--- canConstruct.js
+++ canConstruct.js
@@ -36,4 +36,42 @@ const allConbination = word => {
 // console.log(allConbination('abcdef'))
 
 
-console.log(canConstruct('abcdew', ['ab', 'abc', 'cd', 'def', 'abcd']))
+// console.log(canConstruct('abcdew', ['ab', 'abc', 'cd', 'def', 'abcd']))
+
+// slice can also be used in string and indexOf is used to ensure the selected words are the prefix of the target
+const canConstruct1 = (target, wordBank) => {
+  if (target === '') return true
+
+  for (let word of wordBank){
+    // to ensure the selected words in the wordBank are prefix to the target
+    if (target.indexOf(word) === 0) {
+      let suffix = target.slice(word.length)
+      if (canConstruct1(suffix, wordBank)) return true
+    }
+  }
+  return false
+}
+
+// console.log(canConstruct1('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))
+
+
+const canConstruct2 = (target, wordBank, memo = {}) => {
+
+  console.log(memo)
+
+  if (target === '') return true
+  if (target in memo) return memo[target]
+
+  for (let word of wordBank){
+    // to ensure the selected words in the wordBank are prefix to the target
+    if (target.indexOf(word) === 0) {
+      let suffix = target.slice(word.length)
+      memo[word] = true
+      if (canConstruct2(suffix, wordBank, memo)) return true
+    }
+  }
+
+  return false
+}
+
+console.log(canConstruct2('abcdew', ['ab', 'abc', 'cd', 'def', 'abcd']))
