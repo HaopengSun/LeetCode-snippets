137b370410d1aa32d7dc82ba2fd29a178c5e0fd6
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-24 12:06:53 -0400

    return whethe the strs in an array can be constructed to the target str

---
+++ canConstruct.js
@@ -0,0 +1,38 @@
+const canConstruct = (target, words) => {
+  if (target){
+    const subWords = allConbination(target)
+    for (let word of words){
+      if (subWords.includes(word)){
+        if (word === target) return true
+        if (canConstruct(remainer(word, target), words)) return true
+      }
+    }
+  }
+}
+
+const remainer = (prefix, word) => {
+  let i = 0
+  let wordArr = word.split('')
+  while(prefix[i] === wordArr[0]){
+    wordArr.shift()
+    i += 1
+  }
+  return wordArr.join('')
+}
+
+// console.log(remainer('abc', 'abcdef'))
+
+const allConbination = word => {
+  const result = []
+  let subWord = ''
+  for(let letter of word){
+    subWord += letter
+    result.push(subWord)
+  }
+  return result
+}
+
+// console.log(allConbination('abcdef'))
+
+
+console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))
