43a5cf102c0db315f3b10bb805efc9d9ed31f3b9
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-24 10:57:39 -0400

    using memo to reduce the time complexity

--- bestSum.js
+++ bestSum.js
@@ -12,3 +12,23 @@ const bestSum = (nums, target) => {
 }
 
 console.log(bestSum([2, 3, 4, 5], 8))
+
+// add memo to reduce the time complexity
+const bestSum1 = (nums, target, memo = {}) => {
+  if (target === 0) return []
+  if (target < 0) return null
+  if (target in memo) return target[memo]
+
+  for (let i = nums.length - 1; i >= 0; i--){
+    let remainer = target - nums[i]
+    let remainerResult = bestSum1(nums, remainer)
+    if (remainerResult !== null) {
+      memo[remainer] = remainerResult
+      return [...remainerResult, nums[i]]
+    }
+  }
+
+  return null
+}
+
+console.log(bestSum1([2, 3, 4, 5], 30))
