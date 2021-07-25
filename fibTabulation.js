0ba11d0bc87af0525404d6c6dec97cbadee2a180
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-25 12:14:14 -0400

    generate an array directly

--- fibTabulation.js
+++ fibTabulation.js
@@ -1,3 +1,4 @@
+// generate memo obj and transfer it to arr
 const fibTabulation = n => {
   const memo = {}
 
@@ -5,7 +6,7 @@ const fibTabulation = n => {
     if (n === 1) return 0
     if (n === 2) return 1
     if (n in memo) return memo[n]
-    memo[n] = fibNum(n - 1) + fibNum(n - 2)
+    memo[n] = fibNum((n - 1), memo) + fibNum((n - 2), memo)
     return memo[n]
   }
 
@@ -16,5 +17,22 @@ const fibTabulation = n => {
   console.log(result)
 }
 
-fibTabulation(30)
+// fibTabulation(30)
 
+// push the new fibnacci number into the array directly
+const fibTabulation1 = n => {
+  const fibArr = [0, 1]
+  if (n < 2) return fibArr[n]
+  for (let i = 2; i < n; i++){
+    fibArr.push(fibArr[i - 1] + fibArr[i - 2])
+  }
+  console.log(fibArr)
+}
+
+fibTabulation1(30)
+
+const table = Array(10)
+console.log(table) // an array of ten empty elements (undefined)
+
+const table0 = Array(10).fill(0)
+console.log(table0) // an array of ten zeros
