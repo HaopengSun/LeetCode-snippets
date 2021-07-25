40a12037b71a64256139d90ee64f895f7bb80a76
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-25 12:52:43 -0400

    return the number of ways from top left to bottum right in a 2d grid using a grid which can represent the number of ways

--- gridTravellerTabulation.js
+++ gridTravellerTabulation.js
@@ -1,7 +1,7 @@
 const gridTravellerTabulation = (m, n, memo={}) => {
   if (m === 0 || n === 0) return 0
   if (m === 1 && n === 1) return 1
-  
+
   let key = m.toString() + n.toString()
   if (key in memo) return memo[key]
 
@@ -9,4 +9,32 @@ const gridTravellerTabulation = (m, n, memo={}) => {
   return memo[key]
 }
 
-console.log(gridTravellerTabulation(10, 10))
+// console.log(gridTravellerTabulation(3, 3))
+
+
+// initialize a 2d grid with value of zero
+// this grid is not the real grid that the traveller can go throught
+// this grid is for showing the count of ways that the grid can go
+
+const gridTravellerTabulation1 = (m, n) => {
+  const grid = []
+  while(m >= 0){
+    grid.push(Array(n + 1).fill(0))
+    m--
+  }
+
+  // going throught a 1 * 1 grid can only have one way
+  grid[1][1] = 1
+  
+  for (let i = 0; i < grid.length; i++){
+    for (let j = 0; j < grid[i].length; j++){
+      let curr = grid[i][j]
+      if (i + 1 < grid.length) grid[i + 1][j] += curr
+      if (j + 1 < grid[i].length) grid[i][j + 1] += curr
+    }
+  }
+
+  console.log(grid)
+}
+
+gridTravellerTabulation1(3, 3)
