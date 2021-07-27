aa9e13d49a6e27ceadecdc8fa2d49ef754ca1926
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-27 12:11:16 -0400

    build tabulation with elements which are array of num

--- howSumTabulation.js
+++ howSumTabulation.js
@@ -1,6 +1,6 @@
 const howSum = (target, nums) => {
   const arr = Array(target + 1).fill(null)
-  arr[0] = '0'
+  arr[0] = ''
   for (let i = 0; i < target + 1; i++){
     if (arr[i] !== null) {
       for (let num of nums){
@@ -8,7 +8,25 @@ const howSum = (target, nums) => {
       }
     }
   }
+  console.log(arr)
   if (arr[target]) return arr[target].split('').map(num => Number(num))
 }
 
-console.log(howSum(6, [2, 3, 4]))
+// console.log(howSum(6, [2, 3, 4]))
+
+const howSum1 = (target, nums) => {
+  const arr = Array(target + 1).fill(null)
+  arr[0] = []
+  for (let i = 0; i < target + 1; i++){
+    if (arr[i] !== null) {
+      for (let num of nums){
+        if (num + i < target + 1) {
+          arr[num + i] = [...arr[i], num]
+        }
+      }
+    }
+  }
+  return arr[target] ? arr[target] : false
+}
+
+console.log(howSum1(6, [2, 3, 4]))
