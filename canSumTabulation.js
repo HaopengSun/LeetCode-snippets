c2ba348500f743b277bd1210d2cd9b34b8eb53be
HaopengSun <sunhaopeng2016@gmail.com>
2021-07-27 11:07:01 -0400

    list all posibilities and filter

--- canSumTabulation.js
+++ canSumTabulation.js
@@ -9,4 +9,32 @@ const canSum = (target, nums) => {
   return false
 }
 
-console.log(canSum(7, [5, 3, 4]))
+// console.log(canSum(7, [5, 3, 4]))
+
+const canSum1 = (target, nums) => {
+  const grid = []
+  for(let i = 0; i < nums.length - 1; i++){
+    grid.push(nums[i])
+    grid.push(nums[i] * 2)
+    for(let j = i + 1; j < nums.length; j++){
+      let sum = nums[i] + nums[j]
+      grid.push(sum)
+    }
+  }
+
+  const found = (target, grid) => {
+    for (let element of grid){
+      if (target === element) return true
+    }
+    for (let element of grid){
+      if (target > element) {
+        if (found((target - element), grid)) return true
+      }
+    }
+    return false
+  }
+
+  return found(target, grid)
+}
+
+console.log(canSum1(7, [5, 3, 4, 2, 5]))
