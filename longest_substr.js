d0f6145e4433cbf252efe1a5abe3ba9a5ebb4517
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-08-25 13:42:38 -0400

    functions for grid traveler and most water

--- longest_substr.js
+++ longest_substr.js
@@ -35,3 +35,34 @@ const fib = (num, memo = {}) => {
 }
 
 console.log(fib(100))
+
+const gridTraveler = (long, short, memo = {}) => {
+  const key = long.toString() + short.toString();
+
+  if (key in memo) return memo[key];
+  if (long === 1 || short === 1) return 1;
+  if (long === 0 || short === 0) return 0;
+
+  memo[key] = gridTraveler(long - 1, short, memo) + gridTraveler(long, short - 1, memo)
+  return memo[key]
+}
+
+console.log(gridTraveler(18, 18))
+
+const mostWater = arr => {
+  let most = 0
+  for (let i = 0; i < arr.length - 1; i++){
+    for (let j = i + 1; j < arr.length; j++){
+      const long = j - i;
+      const height = arr[i] > arr[j] ? arr[j] : arr[i]
+      const area = long * height
+      if (area > most) most = area
+    }
+  }
+  console.log(most)
+}
+
+let height = [4,3,2,1,4]
+mostWater(height)
+height = [1,8,6,2,5,4,8,3,7]
+mostWater(height)
